6c3b610f4831731085a3fb32afe44d33d1b3ab13
Nevilkumar <nevil.0619@gmail.com>
2021-12-22 16:23:35 +0530

    updated

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles.js';
-import { createPost } from '../../actions/posts.js';
+import { createPost, updatePost } from '../../actions/posts.js';
 
-const Form = () => {
+const Form = ({currentId, setCurrentId}) => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post]);
+    
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -14,21 +22,26 @@ const Form = () => {
         tags: '',
         selectedFile: ''
     })
-    const classes = useStyles();
-    const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        dispatch(createPost(postData));
+        if(currentId){
+            dispatch(updatePost(currentId, postData));
+        } else{
+            dispatch(createPost(postData));
+        }
+        clear();
+
     }
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({creator: '', title: '', message: '', tags: '', selectedFile: ''})
     }
     return (    
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant = "h6">Creating a Memory</Typography>
+                <Typography variant = "h6">{currentId? 'Editing' : 'Creating'} a Memory</Typography>
                 <TextField 
                     name="creator" 
                     variant="outlined" 
@@ -59,7 +72,7 @@ const Form = () => {
                     label="tags" 
                     fullWidth
                     value={postData.tags}
-                    onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+                    onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}
                  />
 
                 <div className={classes.fileInput}>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector  } from 'react-redux';
 
 import Post from './Post/Post.js';
 
 import useStyles from './styles';
 
-const Posts = () => {
+const Posts = ({setCurrentId}) => {
     const classes = useStyles();
     const posts = useSelector((state) => state.posts);
 
-    console.log(posts);
     return (
-        <>
-            <h1>POSTS</h1>            
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                { 
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post} setCurrentId={setCurrentId} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     )
 }
 
