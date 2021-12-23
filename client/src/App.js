ea50e6dd277a4610c9da20308bee0a77846344cb
Nevilkumar <nevil.0619@gmail.com>
2021-12-23 11:05:32 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import { Container } from '@material-ui/core';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts.js';
-import Posts from './components/Posts/Posts.js'
-import Form from './components/Form/Form.js'
-import memories from './images/memories.png';
-import useStyles from './styles';
-
+import Home from './components/Home/Home.js';
+import Navbar from './components/Navbar/Navbar.js';
+import Auth from './components/Auth/Auth.js';
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
+    
     return (
-        <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className='classes.heading' variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>   
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
+        <BrowserRouter>
+            <Container maxwidth="lg">
+                <Navbar />
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/auth" exact element={<Auth />} />
+                </Routes>
                 </Container>
-            </Grow>
-        </Container>
+        </BrowserRouter>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
 
 mongoose.connect('mongodb://localhost:27017/MERN', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(8000, () => console.log("Listening On Port 8000")))
